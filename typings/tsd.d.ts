90c660063ef62221562a9c1e8b70c5d5acaf1dea
David Tesar <dtzar@hotmail.com>
2016-12-28 11:19:33 -0800

    Wrap into async run function

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,8 +3,8 @@
 /// <reference path="shelljs/shelljs.d.ts" />
 /// <reference path="vsts-task-lib/vsts-task-lib.d.ts" />
 
-declare module "vsts-task-lib" {
-	export * from 'vsts-task-lib/task';
-	export * from 'vsts-task-lib/taskcommand';
-	export * from 'vsts-task-lib/toolrunner';
-}
+/// declare module "vsts-task-lib" {
+/// export * from 'vsts-task-lib/task';
+/// export * from 'vsts-task-lib/taskcommand';
+/// export * from 'vsts-task-lib/toolrunner';
+/// }
